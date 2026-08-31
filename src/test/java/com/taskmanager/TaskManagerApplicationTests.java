4f4ea44151ec9f66fb6347fb97befd2eb3587505
Saibalaji <saibalajisai24@gmail.com>
2026-08-31 18:11:20 +0530

    Add JUnit test for Jenkins pipeline

---
+++ src/test/java/com/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TaskManagerApplicationTests {
+
+    @Test
+    void applicationTest() {
+        assertTrue(true);
+    }
+}
