20d379a2b85fe29df693ed7113e82769eb9abca2
EC2 Default User <ec2-user@ip-10-0-1-232.ap-south-1.compute.internal>
2026-09-12 05:56:28 +0000

    Add tests for webhook demo

---
+++ src/test/java/com/taskmanager/WebhookTest.java
@@ -0,0 +1,17 @@
+package com.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebhookTest {
+
+    @Test
+    void passingTest() {
+        assertEquals(2, 1 + 1, "Math works!");
+    }
+
+    @Test
+    void failingTest() {
+        assertEquals(42, 1 + 1, "Intentional failure!");
+    }
+}
